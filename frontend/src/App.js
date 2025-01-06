5f6151b8c226b9292cc983d2fe883fad99cc90fc
dee0tf <derrickookoro1234@gmail.com>
2025-01-06 15:54:46 -0800

    good

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Background from './components/background/background';
-import FunFact from './components/fun-fact/fun-fact';
-import Leaderboard from './components/leaderboard/leaderboard';
-import LandingPage from './components/landing-page/landing-page';
-import Username from './components/username/username';
-import GameStart from './components/game-start/game-start';
-import Game from './components/game/game';
 
 function App() {
-  const isMobile = window.innerWidth <= 768;
-
-  if (isMobile) {
-    return (
-      <Background>
-        <div className="not-allowed">
-            <h1>Access Not Allowed</h1>
-            <p>This website is not accessible on mobile devices.</p>
-        </div>
-        </Background>
-    );
-}
-
   return (
     <div className="App">
-      <Background>
-        {/* <FunFact/> */}
-        {/* <Leaderboard/> */}
-        {/* <LandingPage/> */}
-        {/* <Username/> */}
-        {/* <GameStart/> */}
-        <Game/>
-      </Background>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
