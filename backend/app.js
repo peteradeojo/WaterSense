b6e022e22846848bb275a926d5a509712bc71559
Boluwatife <adeojopeter@gmail.com>
2025-01-21 13:11:25 +0100

    logs

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-if (process.env.NODE_ENV != "production") {
-	app.use(morgan("dev"));
-}
+app.use(morgan("dev"));
 
 // Routes
 app.use("/auth", auth());
