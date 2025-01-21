1c7144d03f1c095c65c51d6bdc3017c3fb43f7f8
olayinkaatobiloye <hiqmatolayinka@gmail.com>
2025-01-21 16:08:53 +0100

    fix leaderboard

--- frontend/src/components/leaderboard/leaderboard.js
+++ frontend/src/components/leaderboard/leaderboard.js
@@ -15,11 +15,6 @@ const Leaderboard = () => {
   const { globalState } = useContext(GlobalStateContext);
 
   const [players, setPlayers] = useState([
-    { rank: 1, name: "Fabzy", score: 500, avatar: "avatar1.png", isTop: true },
-    { rank: 2, name: "Fabzy", score: 414, avatar: "avatar2.png" },
-    { rank: 3, name: "Fabzy", score: 408, avatar: "avatar3.png" },
-    { rank: 4, name: "Fabzy", score: 400, avatar: "avatar4.png" },
-    { rank: 5, name: "Fabzy", score: 300, avatar: "avatar5.png" },
   ]);
 
   // Handling socket connection
